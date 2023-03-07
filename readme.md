b10fa217370f57f44859fdea18caf1cac6d9b1ad
Gabriel Orgo <gabriel.orgo@qualitor.com.br>
2023-03-07 00:39:44 -0300

    feat: It's working with vue :)

---
+++ readme.md
@@ -0,0 +1,14 @@
+Tem que setar o diretório de output na ../web
+
+Seguinte, na @vue roda aí um:
+
+```
+  yarn build
+```
+
+Aí depois tu roda
+
+```
+  python ./main.py
+
+```
---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const number = ref<number>(0);
+const ping = ref<boolean>(false);
+
+const click = () => {
+  number.value++;
+  ping.value = true;
+
+  setTimeout(() => {
+    ping.value = false;
+  }, 500);
+};
+</script>
+
+<template>
+  <div class="w-screen h-screen flex items-center justify-center">
+    <div class="flex flex-col items-center rounded-full p-20">
+      <img
+        :class="{ 'animate-ping': ping }"
+        class="w-10 h-10"
+        src="./assets/vue.svg"
+      />
+      <h1>{{ number }}</h1>
+      <button class="py-2 px-4 rounded border" @click="click">Clica?!</button>
+    </div>
+  </div>
+</template>
